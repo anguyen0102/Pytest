d116081a49ec8d480ea27bd6589a734feb794869
Athena Phommivong <aphomnguyen@gmail.com>
2023-04-13 11:18:55 -0500

    added friend,py file

--- starter_test.py
+++ starter_test.py
@@ -16,7 +16,6 @@ import pytest
 from datetime import date
 from friend import *
 
-
 ### unit tests ###
 def test_calculate_current_age():
     """
@@ -26,9 +25,8 @@ def test_calculate_current_age():
     """
     print("\r")  # carriage return
     print(" -- calculate_current_age unit test")
-    assert (
-        calculate_current_age(2000) == 23
-    )  # STATIC: will change as the years progress
+    assert calculate_current_age(2000) == 23
+     # STATIC: will change as the years progress
 
 
 def test_calculate_current_age():
@@ -37,9 +35,26 @@ def test_calculate_current_age():
     WHEN that year is passed to this function
     THEN the user's age is accurately calculated
     """
+
     birth_year = 1995
     today = date.today()
     expected_age = today.year - birth_year
+    print("\r")  # carriage return
+    print(" -- calculate_current_age unit test")
     assert (
         calculate_current_age(birth_year) == expected_age
     )  # DYNAMIC: calculates the current year
+
+
+
+def test_calculate_future_age():
+    """
+    GIVEN a user current agee
+    WHEN that year is passed to this function
+    THEN the user's age is accurately calculated +10
+    """
+    user_age = 20
+    print("\r")  # carriage return
+    print(" -- calculate_future_age unit test")
+    assert calculate_current_age(user_age) == 30
+     # STATIC: will change as the years progress
